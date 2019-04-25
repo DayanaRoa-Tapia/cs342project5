510d2269e222a825d56092db7f84f9b56b1b0adb
EmilyLin8073 <chestnut321@hotmail.com>
2019-04-25 00:29:38 -0500

    Add files via upload

---
+++ Client.js
@@ -0,0 +1,29 @@
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 5555;
+
+var client = new net.Socket();
+
+client.connect(PORT, HOST, function(){
+
+    console.log('CONNECTED to port : ' + PORT);
+    client.write();  //The thing we need to write to the server
+});
+
+//Add data event handler for the client socket
+//Data is what the server sent to this socket
+client.on('data', function(data){
+
+    console.log(); //What server sent to the client
+
+    //Close the client socket
+    client.destroy();
+}
+
+
+//Add close event handler for the client socket
+client.on('close', function()){
+
+    console.log('Connection closed');
+
+}
